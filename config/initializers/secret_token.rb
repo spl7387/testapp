f53cf123d0d6373f5fe907b70ff6aa1bb8e9c018
stephen <spl7387@gmail.com>
2013-10-03 17:34:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siliconroad::Application.config.secret_token = '13dac8907d205d07e1fe8c610c898c64d835c156786e093d40541251959de8bfcbc8bc52e89a527de985c871259fe8355b558450f8b4f44862218e4fe05c2fce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Siliconroad::Application.config.session_store :cookie_store, key: '_siliconroad_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Siliconroad::Application.config.session_store :active_record_store
