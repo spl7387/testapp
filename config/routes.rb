e024efd7ed3ba1c7a01e9b14f772dfeb0dc190e7
stephen <spl7387@gmail.com>
2013-10-03 18:57:20 -0700

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Siliconroad::Application.routes.draw do
+  
+  get 'about' => 'pages#about'
+
+  
+  root :to => 'pages#home'
+
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
