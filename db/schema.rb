bc02b1a6fbb7db095d3a01d7fe456c016950c15e
stephen <spl7387@gmail.com>
2013-10-07 21:49:43 -0700

    fixed up changes

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,16 @@ ActiveRecord::Schema.define(:version => 20131004035008) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "posts_old", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "user_id"
   end
 
-  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
+  add_index "posts_old", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
